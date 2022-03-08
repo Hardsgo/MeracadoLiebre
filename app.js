46d54e5f0f2dc9c1a46422ca13163dfa8e4da66f
Hardsgo <hard.sgo1995@gmail.com>
2022-03-08 17:47:59 -0500

    cambio de puerto

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const port = process.env.PORT || 3000
 // app.use('/static', express.static(__dirname + '/public'));
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3000, ()=>{
-    console.log('Server is runing');
-});
+app.listen(port, () =>
+  console.log(`Mercado Liebre Server corriendo en puerto ${port}` )
+);
+
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
